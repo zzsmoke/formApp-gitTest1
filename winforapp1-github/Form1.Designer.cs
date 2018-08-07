995959e87ae88c623cc8d2103998f866610e518f
zzsmoke <smoke2206@gmail.com>
2018-08-07 16:46:46 -0300

    2 botones agregados...

--- winforapp1-github/Form1.Designer.cs
+++ winforapp1-github/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.B_btn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // B_btn
@@ -41,11 +43,31 @@
             this.B_btn.UseVisualStyleBackColor = true;
             this.B_btn.Click += new System.EventHandler(this.B_btn_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(326, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(326, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(767, 368);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.B_btn);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +78,8 @@
         #endregion
 
         private System.Windows.Forms.Button B_btn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
