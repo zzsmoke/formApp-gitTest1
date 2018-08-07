d1f4afd99f12a7186103a73c001246a5e8f186a4
zzsmoke <smoke2206@gmail.com>
2018-08-07 16:34:15 -0300

    primer boton y mensaje...

---
+++ winforapp1-github/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace winforapp1_github
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.B_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // B_btn
+            // 
+            this.B_btn.Location = new System.Drawing.Point(326, 166);
+            this.B_btn.Name = "B_btn";
+            this.B_btn.Size = new System.Drawing.Size(75, 23);
+            this.B_btn.TabIndex = 0;
+            this.B_btn.Text = "Boton";
+            this.B_btn.UseVisualStyleBackColor = true;
+            this.B_btn.Click += new System.EventHandler(this.B_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 368);
+            this.Controls.Add(this.B_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B_btn;
+    }
+}
+
---
+++ winforapp1-github/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winforapp1_github
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void B_btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola mundo");
+        }
+    }
+}
